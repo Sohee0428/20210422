6bbb9f4e520d18e22afe317b2f9627601686071b
Sohee <sohee3480@gmail.com>
2021-04-22 16:55:15 +0900

    로그인 버튼 클릭 시 토스트 사용하기

--- app/src/main/java/com/example/a20210422/MainActivity.kt
+++ app/src/main/java/com/example/a20210422/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.a20210422
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Btn.setOnClickListener {
+
+            val id = edtId.text.toString()
+            val password = edtPassword.text.toString()
+
+            if (id == "sohee@naver.com" && password == "0428") {
+                Toast.makeText(this, "권소희 관리자입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
